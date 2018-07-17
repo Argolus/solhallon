b1f3b7e7865de369c37ba1f39c4228d519954686
Argolus <40291329+Argolus@users.noreply.github.com>
2018-07-17 09:49:57 +0200

    Rensning
    Tagit bort fyra överflödiga filer

--- SOL_1_Max32/Sensor.ino
+++
@@ -1,7 +0,0 @@
-boolean IsEqualSensorID(byte Sensor1[], byte Sensor2[], int LengthOfID){
-  boolean IsEqual = false; 
-  do{
-    LengthOfID--;
-    IsEqual = Sensor1[LengthOfID] == Sensor2[LengthOfID];
-  }while(LengthOfID>0 && IsEqual);
-}    
